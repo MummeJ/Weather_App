f2bf9c712aa1204f797056437a8fa23cb4b17ab2
Jeremy <mummejjeremy@gmail.com>
2021-09-08 14:56:16 -0500

    Added location and weather api functionality

---
+++ flaskr/api_request.py
@@ -0,0 +1,22 @@
+import requests
+import geocoder
+
+api_key = 'c1a0b5df07dfd6f81425799dbbd1c044'
+
+def k_to_f(k_temp):
+    fahrenheit = (k_temp - 273.15) * 9/5 + 32
+    return fahrenheit
+
+def get_location():
+    g = geocoder.ip('me')
+    return g.city, g.state
+
+def get_weather(city, state):
+    r = requests.get('https://api.openweathermap.org/data/2.5/weather?q={},MS,US&appid={}'.format(str(city), api_key))
+    dict = r.json()
+    current_temp = str(round(k_to_f(dict['main']['temp']), 1))
+    feels_like = str(round(k_to_f(dict['main']['feels_like']), 1))
+    temp_high = str(round(k_to_f(dict['main']['temp_max']), 1))
+    temp_low = str(round(k_to_f(dict['main']['temp_min']), 1))
+    sky = dict['weather'][0]['description']
+    return current_temp, feels_like, temp_high, temp_low, sky
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -2,9 +2,13 @@ import functools
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
+from flaskr.api_request import get_location, get_weather
 
 bp = Blueprint('routes', __name__, url_prefix='/')
 
 @bp.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    location = get_location()
+    current_weather = get_weather(location[0], location[1])
+    #return render_template('index.html',location=location, #weather=current_weather)
+    return render_template('index.html', location=location, weather=current_weather)

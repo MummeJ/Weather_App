b4c294bf1f86dd7760d4b49c5ae670ee4dadec9b
Jeremy <mummejjeremy@gmail.com>
2021-09-10 18:02:19 -0500

    Added coordinate api fuciton

--- flaskr/api_request.py
+++ flaskr/api_request.py
@@ -2,7 +2,8 @@ import requests
 import geocoder
 from flask import redirect
 
-api_key = 'c1a0b5df07dfd6f81425799dbbd1c044'
+google_key = 'AIzaSyDc3GdOMZnLnRGztSGHRe9FGATaMEfrSO0'
+owm_key = 'c1a0b5df07dfd6f81425799dbbd1c044'
 
 def k_to_f(k_temp):
     fahrenheit = (k_temp - 273.15) * 9/5 + 32
@@ -15,13 +16,39 @@ def get_location():
     else:
         return "Unavailable"
 
-
-def get_weather(city, state):
-    r = requests.get('https://api.openweathermap.org/data/2.5/weather?q={},{},US&appid={}'.format(city, state, api_key))
+def get_coordinates(city, state):
+    params = {'key': google_key, 'address': city + ', ' + state}
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    r = requests.get(base_url, params=params)
     dict = r.json()
-    current_temp = str(round(k_to_f(dict['main']['temp']), 1))
-    feels_like = str(round(k_to_f(dict['main']['feels_like']), 1))
-    temp_high = str(round(k_to_f(dict['main']['temp_max']), 1))
-    temp_low = str(round(k_to_f(dict['main']['temp_min']), 1))
+    if dict['status'] == 'OK':
+        geometry = dict['results'][0]['geometry']
+        lat = geometry['location']['lat']
+        lon = geometry['location']['lng']
+        return lat, lon
+
+def get_future_weather(city, state):
+    coordinates = get_coordinates(city, state)
+    params = {'lat': coordinates[0], 'lon':coordinates[1], 'units': 'imperial', 'exclude': 'current+minutely', 'appid': owm_key }
+    base_url = 'https://api.openweathermap.org/data/2.5/onecall?'
+    r = requests.get(base_url, params=params)
+    dict = r.json()
+    current_temp = str(round(dict['current']['temp']))
+    feels_like = str(round(dict['current']['feels_like']))
+    temp_high = str(round(dict['current']['pressure']))
+    temp_low = str(round(dict['current']['humidity']))
+    sky = str(round(dict['current']['humidity']))
+    return current_temp, feels_like, temp_high, temp_low, sky
+
+def get_current_weather(city, state):
+    city_state = city + ',' + state + ',US'
+    params = {'q': city_state, 'units': 'imperial', 'appid': owm_key }
+    base_url = 'https://api.openweathermap.org/data/2.5/weather?'
+    r = requests.get(base_url, params=params)
+    dict = r.json()
+    current_temp = str(round(dict['main']['temp']))
+    feels_like = str(round(dict['main']['feels_like']))
+    temp_high = str(round(dict['main']['temp_max']))
+    temp_low = str(round(dict['main']['temp_min']))
     sky = dict['weather'][0]['description']
     return current_temp, feels_like, temp_high, temp_low, sky
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -2,7 +2,7 @@ import functools
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
-from flaskr.api_request import get_location, get_weather
+from flaskr.api_request import get_location, get_current_weather
 from flaskr.forms import SearchForm
 
 bp = Blueprint('routes', __name__)
@@ -11,7 +11,7 @@ bp = Blueprint('routes', __name__)
 def index():
     location = get_location()
     if location != 'Unavailable':
-        current_weather = get_weather(location[0], location[1])
+        current_weather = get_current_weather(location[0], location[1])
         form = SearchForm(csrf_enabled=False)
         if request.method == 'POST':
             if form.validate_on_submit():
@@ -41,9 +41,8 @@ def search(city, state):
             city = city.capitalize()
             state = state.upper()
             return redirect('/search/'+city+'_'+state)
-    #return render_template('search.html', form=form, location=location, weather=current_weather)
     try:
-        current_weather = get_weather(city, state)
+        current_weather = get_current_weather(city, state)
     except:
         flash('Sorry, we could not find the weather for '+city+', '+state+'. Try searching again!')
         return redirect(url_for('routes.index'))

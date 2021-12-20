6b8682ee0e6dafc0773beabb343ceb8b139f8b5a
Jeremy Mumme <jeremym@completenetwork.com>
2021-12-20 14:50:58 -0600

    Added procfile and wsgi file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='fNcgi1wF8ZIv4K1g25GW-Q',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
